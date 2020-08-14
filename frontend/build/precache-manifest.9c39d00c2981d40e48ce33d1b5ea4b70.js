c61758e78ab7911cda57ff7b4b2c7371aa220f11
Fernando Aldair <ferpool_ald@hotmail.com>
2020-08-14 12:37:07 -0500

    fix mistakes

---
+++ frontend/build/precache-manifest.9c39d00c2981d40e48ce33d1b5ea4b70.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61205bf8770def4d67d064890074c336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72c8030f8188a12f196",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c6be24b4cb46a591bcc8",
+    "url": "/static/css/main.d3db8f29.chunk.css"
+  },
+  {
+    "revision": "e72c8030f8188a12f196",
+    "url": "/static/js/2.a54c1167.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a54c1167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6be24b4cb46a591bcc8",
+    "url": "/static/js/main.5c234689.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "f8a901ce3be8cbcabd9dc343fb7b6c40",
+    "url": "/static/media/hero-image.f8a901ce.svg"
+  },
+  {
+    "revision": "ec8d5b495acb0df78571dc9d0cc7149c",
+    "url": "/static/media/logod.ec8d5b49.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  }
+]);
