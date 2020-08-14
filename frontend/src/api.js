798b18a281c4656e3cf78ef0300b0c070426111e
Fernando Aldair <ferpool_ald@hotmail.com>
2020-08-14 13:23:01 -0500

    fix login

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://servicemante.ga';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://servicemante.ga';
+// const BASE_URL = 'http://localhost:8000';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
